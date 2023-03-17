cf4408ab00d9187d751a934ed0cc51c4927b8cbc
azur <natapat.samutpong@gmail.com>
2023-03-18 05:55:49 +0700

    hooray

---
+++ nixos/boot.nix
@@ -0,0 +1,5 @@
+{ inputs, outputs, lib, config, pkgs, ... }: {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+}
